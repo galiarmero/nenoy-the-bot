5c3152da4bed0a723aa436b91596299eaf6ee214
Gali Armero <galiarmero.dev@gmail.com>
2022-08-25 17:44:11 +0800

    Add initial greeting and help message

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Telegraf } = require('telegraf')
+const gatekeeper = require('./middlewares/gatekeeper')
+
+require('dotenv').config()
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.start((ctx) => {
+    ctx.reply(
+        `Hi, ${ctx.update.message.from.first_name}! I'm Nenoy 🐶, ` +
+        `your personal assistant. I'll be taught many tricks for you in the future.\n\n` +
+        `For now, I'll take care of your puzzle scores. ` +
+        `Just send them to me and I'll ship it into your website! 🚀`
+    )
+})
+bot.help((ctx) => ctx.reply('Send me your puzzle results and I\'ll ship them into your website! 🚀'))
+
+bot.on('message', function (ctx, next) {
+    console.log(ctx)
+    ctx.telegram.sendMessage(ctx.message.chat.id,
+      "Howdy"
+    )
+})
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
