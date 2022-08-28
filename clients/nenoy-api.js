f93e24cb8077f1a0cc65c0f8fdd0fb3460c11919
Gali Armero <galiarmero.dev@gmail.com>
2022-08-28 14:20:16 +0800

    Use path instead of full url

--- clients/nenoy-api.js
+++ clients/nenoy-api.js
@@ -8,9 +8,6 @@ const {
 } = require('../middlewares/interceptors')
 
 const NENOY_API_BASE_URL = process.env.NENOY_API_BASE_URL ?? 'http://127.0.0.1:3000'
-const PUZZLE_SCORES_URL = `${NENOY_API_BASE_URL}/puzzle-scores`
-
-
 const nenoyApi = axios.create({
     baseURL: NENOY_API_BASE_URL,
     timeout: 1000,
@@ -19,7 +16,7 @@ nenoyApi.interceptors.request.use(requestInterceptor, requestErrorInterceptor)
 nenoyApi.interceptors.response.use(responseInterceptor, responseErrorInterceptor)
 
 const addPuzzleScore = async (newPuzzleScore) => {
-    return nenoyApi.post(PUZZLE_SCORES_URL, newPuzzleScore);
+    return nenoyApi.post('/puzzle-scores', newPuzzleScore);
 }
 
 module.exports.addPuzzleScore = addPuzzleScore
