e928ad7c2eaadd3037e361f27d4f62d1b86be656
Gali Armero <galiarmero.dev@gmail.com>
2022-10-13 13:51:56 +0800

    Improve logging

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,12 @@
 const { deduceMessageType, getHandler } = require('./concierge')
 
 const mainController = async (ctx) => {
-    console.log(ctx)
     const type = deduceMessageType(ctx)
     handle = getHandler(type)
+
+    const { text, from } = ctx.update.message
+    console.log(`New message of type ${type} from ${from.id} (${from.username}): '${text}'`)
+
     await handle(ctx)
 }
 
