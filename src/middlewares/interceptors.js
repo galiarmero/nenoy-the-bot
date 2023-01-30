9737cb83dd3a9d31ee9a4603a2a7b2f4b16825e1
Gali Armero <galiarmero.dev@gmail.com>
2023-01-30 11:40:39 +0800

    Fix error handling

--- src/middlewares/interceptors.js
+++ src/middlewares/interceptors.js
@@ -9,32 +9,24 @@ const requestErrorInterceptor = (error) => {
     return Promise.reject(error);
 }
 
-const responseInterceptor = (response) => {
-    return response;
-}
-
-const responseErrorInterceptor = (error) => {
+const identifyCauseType = (error) => {
     if (!error) {
-        error = { causeType: CAUSE.Unknown }
+        return CAUSE.Unknown
     } else if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
-        error.causeType = CAUSE.ErrorResponse
+        return CAUSE.ErrorResponse
     } else if (error.request) {
         console.log(error)
         // The request was made but no response was received
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
-        error.causeType = CAUSE.NoResponse
-    } else {
-        // Something happened in setting up the request that triggered an Error
-        error.causeType = CAUSE.NotSent
+        return CAUSE.NoResponse
     }
 
-    return Promise.reject(error);
+    return CAUSE.NotSent;
 }
 
 module.exports.requestInterceptor = requestInterceptor
 module.exports.requestErrorInterceptor = requestErrorInterceptor
-module.exports.responseInterceptor = responseInterceptor
-module.exports.responseErrorInterceptor = responseErrorInterceptor
+module.exports.identifyCauseType = identifyCauseType
