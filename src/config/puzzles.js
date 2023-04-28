32dc13ac5421a4bd628f6c94ca8f454e5a4255aa
Gali Armero <galiarmero.dev@gmail.com>
2023-04-28 10:53:49 +0800

    Update WhereTaken pattern (#34)

--- src/config/puzzles.js
+++ src/config/puzzles.js
@@ -2,56 +2,67 @@ module.exports = {
   wordle: {
     displayName: "Wordle",
     emoji: '🟩',
-    link: 'https://www.nytimes.com/games/wordle/index.html'
+    link: 'https://www.nytimes.com/games/wordle/index.html',
+    resultPattern: /^Wordle\s*\d+/,
   },
   quordle: {
     displayName: "Quordle",
     emoji: '🔢',
-    link: 'https://www.merriam-webster.com/games/quordle/#/'
+    link: 'https://www.merriam-webster.com/games/quordle/#/',
+    resultPattern: /^Daily Quordle\s*\d+/,
   },
   worldle: {
     displayName: "Worldle",
     emoji: '🌍',
-    link: 'https://worldle.teuteuf.fr/'
+    link: 'https://worldle.teuteuf.fr/',
+    resultPattern: /^\#Worldle\s*\#\d+/,
   },
   saltong: {
     displayName: "Saltong",
     emoji: '🇵🇭',
-    link: 'https://saltong.carldegs.com/'
+    link: 'https://saltong.carldegs.com/',
+    resultPattern: /^Saltong\s*\d+/,
   },
   "saltong-mini": {
     displayName: "Saltong Mini",
     emoji: '🇵🇭',
-    link: 'https://saltong.carldegs.com/mini'
+    link: 'https://saltong.carldegs.com/mini',
+    resultPattern: /^Saltong Mini\s*\d+/,
   },
   waffle: {
     displayName: "Waffle",
     emoji: '🧇',
-    link: 'https://wafflegame.net/'
+    link: 'https://wafflegame.net/',
+    resultPattern: /^\#waffle\d+\s*/,
   },
   "nytimes-mini-crossword": {
     displayName: "NY Times Mini",
     emoji: '📰',
-    link: 'https://www.nytimes.com/crosswords/game/mini'
+    link: 'https://www.nytimes.com/crosswords/game/mini',
+    resultPattern: /^https:\/\/www.nytimes.com\/badges\/games\/mini.html*/,
   },
   octordle: {
     displayName: "Octordle",
     emoji: '🐙',
-    link: 'https://octordle.com/daily'
+    link: 'https://octordle.com/daily',
+    resultPattern: /^Daily Octordle\s*\#\d+/,
   },
   "octordle-sequence": {
     displayName: "Sequence Octordle",
     emoji: '🐙',
-    link: 'https://octordle.com/daily-sequence'
+    link: 'https://octordle.com/daily-sequence',
+    resultPattern: /^Daily Sequence Octordle\s*\#\d+/,
   },
   "octordle-rescue": {
     displayName: "Rescue Octordle",
     emoji: '🐙',
-    link: 'https://octordle.com/daily-rescue'
+    link: 'https://octordle.com/daily-rescue',
+    resultPattern: /^Daily Rescue Octordle\s*\#\d+/,
   },
   wheretaken: {
     displayName: "WhereTaken",
     emoji: '📸',
-    link: 'https://wheretaken.teuteuf.fr/'
+    link: 'https://wheretaken.teuteuf.fr/',
+    resultPattern: /^📷 \#WhereTaken🌎\s*\#\d+/,
   },
 }
