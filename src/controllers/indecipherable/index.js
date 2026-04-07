8513b8c3d6ddaeb383c91e31e4236e0809c6e099
Gali Armero <gali.armero@wolt.com>
2026-04-07 10:12:44 +0800

    Log puzzles in message to debug

--- src/controllers/indecipherable/index.js
+++ src/controllers/indecipherable/index.js
@@ -1,8 +1,12 @@
+const PUZZLES = require('../config/puzzles')
+
 const handleIndecipherable = async (ctx) => {
+    const puzzleNames = Object.keys(PUZZLES)
     await ctx.reply(
         'I have been summoned 💪🏼🐶. ' +
         'But I don\'t know what for 😵‍💫\n\n' +
-        'Please press /help.'
+        'Please press /help.\n\n' +
+        `[DEBUG] ${puzzleNames.length} puzzles loaded: ${puzzleNames.join(', ')}`
     )
 }
 
